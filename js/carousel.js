900380677c765e1eae940340cfe1727c502c4de2
bysilvaart <1705.jlss@gmail.com>
2021-09-20 20:01:00 -0300

    dialogues

--- js/carousel.js
+++ js/carousel.js
@@ -2,12 +2,30 @@ const carouselImages = document.querySelector('.carousel');
 const carouselButtons = document.querySelectorAll('.carousel__button');
 const numberOfImages = document.querySelectorAll('.carousel .img').length;
 var widthFrame = 620;
-let dialogue = ["","Hola","voy a contarte", "mi historia","para el reto"];
+let dialogue = ["","¡Hola!<br>Como puedes notar me veo como una persona normal",
+"A quien le gusta mucho usar su teléfono",
+"Pero...<br>¿Qué pensarías si te dijera que esto es un arma?",
+"Y ¿Qué pensarías si te digo que nunca he dormido?",
+"¿Seguirias pensando que soy normal?",
+"Nací con un cerebro diferente al del resto.",
+"Yo no necesito dormir.",
+"Asi que desde pequeño he tenido mucho más tiempo que los demás para hacer más cosas.",
+"Jugué mucho más, comí mucho más y por supuesto, aprendí mucho más.",
+"Inventé un sistema operativo con el cuál puedo hacer muchas cosas.<br>Digamos que mi smartphone es el mejor espía del planeta.",
+"Como podrás adivinar, descubrí muchas cosas. Cosas que no me creerías si te las dijera.",
+"Cosas que deben ser detenidas.",
+"Alguien debe hacer algo.",
+"Fue lo primero que pensé al enterarme.",
+"Pero nadie hacía nada.",
+"Ahora me he convertido en una combinación de Batman con Mr. Robot.",
+"Aunque muchas veces me siento como Kira.",
+"Con mi teléfono y mi notebook hago que los criminales más grandes...",
+"...mueran."];
 let imageIndex = 1;
 let translateX = 0;
 
-let text = "Hola";
-    document.getElementById("text").innerHTML = text;
+
+    document.getElementById("text").innerHTML = dialogue[1];
 
 carouselButtons.forEach(button => {
   button.addEventListener('click', (event) => {
